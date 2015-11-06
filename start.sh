943bb8535135e2134a787b39c6fd4c9af7b68f80
chubbyjiang <chubbyjiang@wooyou.com.cn>
2015-11-06 15:24:53 +0800

    first commit

---
+++ start.sh
@@ -0,0 +1,12 @@
+docker rm -f approot
+docker rm -f nginx_config_root
+docker rm -f nginx_log_root
+docker rm -f php_config_root
+docker rm -f php-fpm
+docker rm -f nginx
+docker run --name=approot -v /home:/var/www/html -d dockerhub:5000/busybox echo approot
+docker run --name=nginx_config_root -v /home/docker/config/nginx/conf.d:/etc/nginx/conf.d -d dockerhub:5000/busybox echo nginx_config_root
+docker run --name=nginx_log_root -v /home/docker/log/nginx/:/var/log/nginx -d dockerhub:5000/busybox echo nginx_log_root
+docker run --name=php_config_root -v /home/docker/config/php:/usr/local/etc/php -d dockerhub:5000/busybox echo php_config_root
+docker run --name=php-fpm  --volumes-from approot --volumes-from php_config_root -d --add-host localhost:172.17.42.1 --add-host amazontest:54.223.196.153 dockerhub:5000/php-fpm
+docker run --name=nginx --volumes-from approot --volumes-from nginx_config_root  --volumes-from nginx_log_root --link php-fpm:php -p 8081:8081 -p 8484:8484 -p 80:80 -d dockerhub:5000/nginx
