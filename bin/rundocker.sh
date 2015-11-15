e81b7d5e81618da7017ccdd7b40ede1d68b08c08
iamazhi <chencongzhi520@gmail.com>
2015-11-15 02:38:56 +0000

    add it

---
+++ bin/rundocker.sh
@@ -0,0 +1,18 @@
+#! /bin/bash 
+docker restart mongodb 
+docker restart openldap
+docker restart case-service
+docker restart tomcat-avatar 
+docker restart cas-server 
+docker restart token-service  
+docker restart nginx  
+docker restart php-fpm 
+docker restart  tomcat-org 
+docker restart case_ui_service 
+docker restart school_service_huiadmin  
+ docker restart verification_service_hjy  
+docker restart hjy-job-service 
+docker restart hjy_ui_server 
+docker restart tomcat-employee    
+docker restart tomcat-account 
+
