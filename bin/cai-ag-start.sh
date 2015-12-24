48209a32d46a0a41d727da2b811a0e1ffc4f067f
root <root@ip-172-31-7-48.cn-north-1.compute.internal>
2015-12-24 07:05:07 +0000

    Adjust:

--- bin/cai-ag-start.sh
+++ bin/cai-ag-start.sh
@@ -5,7 +5,7 @@ docker rm -f php_config_root
 docker rm -f php-fpm
 docker rm -f nginx
 docker run --name=approot -v /home:/var/www/html -d dockerhub:5000/busybox echo approot
-docker run --name=nginx_config_root -v /home/docker/config/nginx/conf.d:/etc/nginx/conf.d -d dockerhub:5000/busybox echo nginx_config_root
+docker run --name=nginx_config_root -v /home/docker/config/nginx:/etc/nginx -d dockerhub:5000/busybox echo nginx_config_root
 docker run --name=nginx_log_root -v /home/docker/log/nginx/:/var/log/nginx -d dockerhub:5000/busybox echo nginx_log_root
 docker run --name=php_config_root -v /home/docker/config/php:/usr/local/etc/php -d dockerhub:5000/busybox echo php_config_root
 docker run --name=php-fpm  --volumes-from approot --volumes-from php_config_root -d --add-host localhost:172.17.42.1 dockerhub:5000/php-fpm
