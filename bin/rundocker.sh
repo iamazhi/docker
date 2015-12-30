915b73ea1cc7b22581ec73acfca15f577d50408d
root <root@ip-172-31-12-39.cn-north-1.compute.internal>
2015-12-30 20:50:48 +0800

    Adjust:

--- bin/rundocker.sh
+++ bin/rundocker.sh
@@ -1,18 +1,14 @@
 #! /bin/bash 
-docker restart mongodb 
-docker restart openldap
-docker restart case-service
 docker restart tomcat-avatar 
-docker restart cas-server 
 docker restart token-service  
 docker restart nginx  
 docker restart php-fpm 
 docker restart  tomcat-org 
-docker restart case_ui_service 
-docker restart school_service_huiadmin  
- docker restart verification_service_hjy  
-docker restart hjy-job-service 
-docker restart hjy_ui_server 
-docker restart tomcat-employee    
-docker restart tomcat-account 
-
+docker restart CAS 
+docker restart cgj_ui_server
+docker restart advanced 
+docker restart tomcat-jur-ui
+docker restart cgj_jurisdiction
+docker restart tomcat-org-ui
+docker restart tomcat-jur-ui
+docker restart omnirest
