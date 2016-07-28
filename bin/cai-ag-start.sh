de2d6c5fe7f6b737ffad2ce45575a924a63afdc1
root <root@ip-172-31-11-71.cn-north-1.compute.internal>
2016-07-28 14:14:42 +0800

    refactory nginx config by cangtai

--- bin/cai-ag-start.sh
+++ bin/cai-ag-start.sh
@@ -13,4 +13,4 @@ docker run --restart=always --name=php_log_root -v /home/docker/log/php/:/var/lo
 docker run --restart=always --name=php_config_root -v /home/docker/config/php:/usr/local/etc -d dockerhub:5000/busybox echo php_config_root
 docker run --restart=always --name=memcache -p 11211:11211 -d dockerhub:5000/memcached
 docker run --restart=always --name=php-fpm  --volumes-from approot --volumes-from php_config_root --volumes-from php_log_root -d --add-host localhost:172.17.42.1 --link memcache:memcache dockerhub:5000/php-fpm
-docker run --restart=always --name=nginx --volumes-from approot --volumes-from nginx_config_root  --volumes-from nginx_log_root --link php-fpm:php -p 8081:8081 -p 8180:8180 -p 8082:8082 -p 8484:8484 -p 5566:5566 -p 80:80 -d dockerhub:5000/nginx
+docker run --restart=always --name=nginx --volumes-from approot --volumes-from nginx_config_root  --volumes-from nginx_log_root --link php-fpm:php -p 8081:8081 -p 8180:8180 -p 8082:8082 -p 8484:8484 -p 5566:5566 -d dockerhub:5000/nginx
