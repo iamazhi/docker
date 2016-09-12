bad6024c413e985276ee1002686deaf7b6c50229
iamazhi <chencongzhi520@gmail.com>
2016-09-12 14:32:55 +0800

    Adjust

---
+++ bin/collector_chmod.sh
@@ -0,0 +1,5 @@
+ chmod -R 777 /home/collectorEPS/www/data
+ chmod -R 777 /home/collectorEPS/www/tmp
+ chmod -R 777 /home/collectorService/serviceEPS/www/data
+ chmod -R 777 /home/collectorService/serviceEPS/www/tmp
+ chmod -R 777 /home/collectorAG/log
---
+++ bin/collector_server.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+nohup sh /home/collector_service_check/ok.sh &
